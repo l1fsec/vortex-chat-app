85d02ec3192f316fa272ba5b772679946f77be3a
l1fsec <54239712+l1fsec@users.noreply.github.com>
2022-01-06 16:07:11 +0100

    bruhmomented#2

---
+++ app.js
@@ -0,0 +1,18 @@
+//App.js
+const express = require('express');
+const app = express();
+//Socket.io has to use the http server
+const server = require('http').Server(app);
+
+//Express View Engine for Handlebars
+const exphbs  = require('express-handlebars');
+app.engine('handlebars', exphbs());
+app.set('view engine', 'handlebars');
+
+app.get('/', (req, res) => {
+  res.render('index.handlebars');
+})
+
+server.listen('3000', () => {
+  console.log('Server listening on Port 3000');
+})
