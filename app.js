c149b62276345e02f27ff8fc53878f4d68836cd0
l1fsec <54239712+l1fsec@users.noreply.github.com>
2022-01-06 15:56:48 +0100

    socket.io add

--- app.js
+++ app.js
@@ -4,10 +4,18 @@ const app = express();
 // define usage of socket.io with http 
 const server = require('http').Server(app);
 
+//socket.io 
+const io = require('socket.io') (server);
+io.on("connection", (socket) => {
+  console.log("User connected");
+})
+
 // express handlebars
 const exphbs = require('express-handlebars')
 app.engine('handlebars', exphbs());
 app.set('view engine', 'handlebars');
+//search for public folder
+app.use('/public', express.static('public'))
 
 app.get('/', (req, res) => {
   res.render('index.handlebars');
