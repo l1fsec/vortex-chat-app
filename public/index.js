4d55ab606ff25cbc47dc2a145b40a8a74ea218c7
l1fsec <54239712+l1fsec@users.noreply.github.com>
2022-01-09 15:13:00 +0100

    Stylování :100:
    Stylování chatu, zatím nic moc :apple:

--- public/index.js
+++ public/index.js
@@ -1,9 +1,12 @@
+$(document).ready( () => {
+  //Connect to the socket.io server
+  const socket = io.connect();
+})
 // index.js
-$(document).ready(() => {
-
+$(document).ready(()=>{
   const socket = io.connect();
 
-  $('#create-user-btn').click((e) => {
+  $('#create-user-btn').click((e)=>{
     e.preventDefault();
     let username = $('#username-input').val();
     if(username.length > 0){
@@ -12,7 +15,6 @@ $(document).ready(() => {
       $('.username-form').remove();
     }
   });
-
   //socket listeners
   socket.on('new user', (username) => {
     console.log(`✋ ${username} has joined the chat! ✋`);
