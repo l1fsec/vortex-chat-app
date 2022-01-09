423e2790ac0c1718fade743ae0746b7904fcb70d
l1fsec <54239712+l1fsec@users.noreply.github.com>
2022-01-09 14:53:09 +0100

    Messaging

--- public/index.js
+++ public/index.js
@@ -1,12 +1,9 @@
-$(document).ready( () => {
-  //Connect to the socket.io server
-  const socket = io.connect();
-})
 // index.js
-$(document).ready(()=>{
+$(document).ready(() => {
+
   const socket = io.connect();
 
-  $('#create-user-btn').click((e)=>{
+  $('#create-user-btn').click((e) => {
     e.preventDefault();
     let username = $('#username-input').val();
     if(username.length > 0){
@@ -15,6 +12,7 @@ $(document).ready(()=>{
       $('.username-form').remove();
     }
   });
+
   //socket listeners
   socket.on('new user', (username) => {
     console.log(`✋ ${username} has joined the chat! ✋`);
