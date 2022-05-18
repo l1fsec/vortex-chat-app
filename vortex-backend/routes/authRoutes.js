1a64708756c26f35f632997c015131e94ce90d9e
l1fsec <54239712+l1fsec@users.noreply.github.com>
2022-05-18 17:46:54 +0200

    Testování middlewaru a logiky tokenů
    Dělal jsem implementaci tokenů, jen by to chtělo nějak testnout, ale vše přes Postmana jede jak má.
    Možná bude příště lepší udělat si jen maličkou stránku, než to testovat přes Postmana.
    
    Mělo by to po testu vyhazovat http://localhost:3000/api/auth/test ⬅

--- vortex-backend/routes/authRoutes.js
+++ vortex-backend/routes/authRoutes.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const authControllers = require('../controllers/auth/authControllers');
 const Joi = require('joi');
 const validator = require('express-joi-validation').createValidator({});
+const auth = require('../middleware/auth');
 
 const registerSchema = Joi.object({
     username: Joi.string().min(3).max(15).required(),
@@ -16,8 +17,12 @@ const loginSchema = Joi.object({
 
 });
 
-
 router.post("/register", validator.body(registerSchema), authControllers.controllers.postRegister);
 router.post("/login", validator.body(loginSchema), authControllers.controllers.postLogin);
+
+//test middleware
+router.get('/test', auth, (req, res) => {
+    res.send("request passed");
+});
 
 module.exports = router;
